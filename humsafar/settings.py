9df4c139f8d3d4c9619418f4076169206f75c7f5
rajkotiya <69031687+rajkotiya@users.noreply.github.com>
2021-04-15 15:07:00 +0530

    removing credentials

--- humsafar/settings.py
+++ humsafar/settings.py
@@ -131,13 +131,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # }
 EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.net'
-<<<<<<< HEAD
-EMAIL_HOST_USER = 
-EMAIL_HOST_PASSWORD = 
-=======
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
->>>>>>> 437fd1e5cf4914067e0c800341e9e5a1a5d15b57
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = ''
